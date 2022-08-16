3b9c260ab121dedeaa32c3f12a9c7c139d604e49
Süeda Özkaya <sozkaya2635@gmail.com>
2022-08-16 22:59:50 +0300

    mnbm

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,6 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("Gitignore Deneme 1");
+        System.out.println("Gitignore Deneme 2");
     }
 }
